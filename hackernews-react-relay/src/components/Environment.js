c0f426518fb68bdce8ac452766e03686eda87ccf
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-12-01 18:08:29 +0100

    get api endpoint

---
+++ hackernews-react-relay/src/components/Environment.js
@@ -0,0 +1,33 @@
+const __RELAY_API_ENDPOINT__ = "shared-eu-west-1/cjao3w8943hga0146fjg36yn0";
+
+ const {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} = require('relay-runtime')
+
+const store = new Store(new RecordSource())
+
+const network = Network.create((operation, variables) => {
+  return fetch(__RELAY_API_ENDPOINT__, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+})
+
+const environment = new Environment({
+  network,
+  store,
+})
+
+export default environment
