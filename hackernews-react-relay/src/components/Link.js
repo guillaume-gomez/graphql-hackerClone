4568c2b19a1d39b35c71377eb6edf93dc9d04848
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-02 15:26:10 +0100

    update render functions

--- hackernews-react-relay/src/components/Link.js
+++ hackernews-react-relay/src/components/Link.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { GC_USER_ID } from '../Constants';
+import { timeDifferenceForDate } from '../utils';
 import {
   createFragmentContainer,
   graphql
@@ -9,12 +10,21 @@ import {
 class Link extends Component {
 
   render() {
+    const userId = localStorage.getItem(GC_USER_ID)
     return (
-      <div>
-        <div>{this.props.link.description} ({this.props.link.url})</div>
+      <div className='flex mt2 items-start'>
+        <div className='flex items-center'>
+          <span className='gray'>{this.props.index + 1}.</span>
+          {userId && <div className='ml1 gray f11' onClick={() => this._voteForLink()}>▲</div>}
+        </div>
+        <div className='ml1'>
+          <div>{this.props.link.description} ({this.props.link.url})</div>
+          <div className='f6 lh-copy gray'>{this.props.link.votes.count} votes | by {this.props.link.postedBy ? this.props.link.postedBy.name : 'Unknown'} {timeDifferenceForDate(this.props.link.createdAt)}</div>
+        </div>
       </div>
     );
   }
+
 
   _voteForLink = async () => {
     // ... you'll implement this in chapter 6
@@ -28,5 +38,13 @@ export default createFragmentContainer(Link, graphql`
     id
     description
     url
+    createdAt
+    postedBy {
+      id
+      name
+    }
+    votes {
+      count
+    }
   }
 `);
