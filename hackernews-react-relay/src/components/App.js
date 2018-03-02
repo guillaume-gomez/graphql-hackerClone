880e9f204189b73095b617ab9a966eed2f35e2dd
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-02 11:57:26 +0100

    use react router

--- hackernews-react-relay/src/components/App.js
+++ hackernews-react-relay/src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import LinkListPage from './LinkListPage'
+import { Switch, Route } from 'react-router-dom'
+
+import Header from './Header'
+import LinkListPage from './LinkListPage';
+import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
     return (
-      <LinkListPage />
-    )
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={LinkListPage}/>
+            <Route exact path='/create' component={CreateLink}/>
+           </Switch>
+        </div>
+      </div>
+    );
   }
 }
 
